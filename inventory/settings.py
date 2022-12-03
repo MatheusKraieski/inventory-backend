3644fcf5274fd4f90112a7faa77578f96f283043
Matheus Kraieski <89086545+MatheusKraieski@users.noreply.github.com>
2022-12-03 14:04:50 -0300

    chore: change settings.py

--- inventory/settings.py
+++ inventory/settings.py
@@ -131,9 +131,9 @@ MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
-django_heroku.settings(locals())
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+django_heroku.settings(locals())

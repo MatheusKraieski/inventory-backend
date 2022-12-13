78907e9ac57f7f81c1dbf67e7404a1f5136aedda
Matheus <89086545+MatheusKraieski@users.noreply.github.com>
2022-12-13 14:47:30 -0300

    chore: delete default numbers

--- apps/products/models.py
+++ apps/products/models.py
@@ -4,11 +4,11 @@ from apps.categories.models import Category
 
 class Product(models.Model):
     name = models.CharField(max_length=255)
-    price = models.DecimalField(decimal_places=2, max_digits=12, default=100)
+    price = models.DecimalField(decimal_places=2, max_digits=12,)
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
-    cost = models.DecimalField(max_digits=12, decimal_places=2, default=100)
-    promotion_price = models.DecimalField(max_digits=12, decimal_places=2, default=100)
-    inventory_number = models.DecimalField(decimal_places=2, max_digits=12, default=100)
-    minimum_amount = models.DecimalField(decimal_places=2, max_digits=12, default=100)
+    cost = models.DecimalField(max_digits=12, decimal_places=2,)
+    promotion_price = models.DecimalField(max_digits=12, decimal_places=2)
+    inventory_number = models.DecimalField(decimal_places=2, max_digits=12)
+    minimum_amount = models.DecimalField(decimal_places=2, max_digits=12)
     image_product = models.ImageField(max_length=100, null=True, blank=True, upload_to="uploads/products")
     favorite = models.BooleanField(default=False)

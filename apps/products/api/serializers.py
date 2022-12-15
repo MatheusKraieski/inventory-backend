b5812b9a6cef93b17afc3ff52e5f48981436fa87
Matheus <89086545+MatheusKraieski@users.noreply.github.com>
2022-12-15 11:42:01 -0300

    chore: create function "create_product"

--- apps/products/api/serializers.py
+++ apps/products/api/serializers.py
@@ -1,8 +1,23 @@
 from rest_framework import serializers
 from apps.products import models
+from apps.products.models import Product
 
-
-class ProductSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = models.Product
-        fields = '__all__'
+class ProductSerializer:
+    
+    def create_product(self, request):
+        try:
+            Product.objects.create(
+                name=request.data.get('name'),
+                price=request.data.get('price'),
+                category=request.data.get('category'),
+                cost=request.data.get('cost'),
+                promotion_price=request.data.get('promotion_price'),
+                inventory_number=request.data.get('inventory_number'),
+                minimum_amount=request.data.get('minimum_amount'),
+                image_product=request.data.get('image_product'),
+                favorite=request.data.get('favorite'),
+            )
+            return {'detail': 'produto criado com sucesso.'}, 201
+        except Exception as e:
+            print(e)
+            return {'detail': 'produto não pode ser criado.'}, 400    
--- apps/products/api/viewsets.py
+++ apps/products/api/viewsets.py
@@ -1,9 +1,26 @@
 from apps.products.models import Product
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from apps.products.api.serializers import ProductSerializer
+
+
+
 
 
 class ProductList(APIView):
     def get(self, request):
         products = Product.objects.values()
         return Response(products, 200)
+
+
+@api_view(['POST'])
+def create_product(request):
+    serializer = ProductSerializer()
+    
+    response, status = serializer.full_validation(request)
+    if status != 200:
+        return Response(response, status)
+    
+    response, status = serializer.create_product(request)
+    return Response(response, status)        

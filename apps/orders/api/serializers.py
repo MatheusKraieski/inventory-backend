c7bd30015b0575a63c65d91100918b50002b5f7d
Matheus <gasparkraieski@gmail.com>
2022-12-18 13:39:38 -0300

    update

--- apps/orders/api/serializers.py
+++ apps/orders/api/serializers.py
@@ -1,17 +1,14 @@
 from apps.orders.models import Order
-from apps.cart.models import Cart
 
 class OrderSerializer:
-    def create_order(self, request):
+    def create_order(self, request, cart):
         try:
-            Order.objects.create(
+            order = Order.objects.create(
                 ref=request.data.get("ref"),
                 payment_type=request.data.get("payment_type"),
                 client_id=request.data.get("client_id")
             )
-            Cart.objects.get(
-                cart_id=request.data.get("cart_id"),
-            )
+            order.add_line_items_to_order(cart)
             return "Order created successfully.", 200
         except Exception as err:
             print(err)

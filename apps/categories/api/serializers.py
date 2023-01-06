623e9edc4486fe35eb329441d21f2d6728e8b673
Matheus <89086545+MatheusKraieski@users.noreply.github.com>
2023-01-06 17:10:30 -0300

    chore: create route post category

--- apps/categories/api/serializers.py
+++ apps/categories/api/serializers.py
@@ -1,8 +1,23 @@
 from rest_framework import serializers
 from apps.categories import models
+from apps.categories.models import Category
 
 
 class CategoriesSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Category
         fields = '__all__'
+
+
+class NewCategory:
+    def create_category(self, request):
+        try:
+            category = Category.objects.create(
+                name=request.data.get('name'),
+                parent=request.data.get("parent"),
+            )
+
+            return {'Category created successfully.'}, 201
+        except Exception as e:
+            print(e)
+            return {'Product could not be created.'}, 400
--- apps/categories/api/viewsets.py
+++ apps/categories/api/viewsets.py
@@ -1,6 +1,7 @@
 from rest_framework import viewsets
 from apps.categories.api import serializers
 from apps.categories.models import Category
+from apps.categories.api.serializers import NewCategory
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
@@ -8,3 +9,9 @@ class CategoryView(APIView):
     def get(self, request):
         categories = Category.objects.values()
         return Response(categories, 200)
+
+    def post(self, request):    
+        serializer = NewCategory()
+            
+        response, status = serializer.create_category(request)
+        return Response(response, status)    

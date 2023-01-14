6b11d74d948415f8805b11f74b936152a8e172fa
Guilherme Bordignon <guilherme.bordignon@cialdnb.com>
2023-01-14 20:59:04 -0300

    chore: fix shit

--- apps/products/api/serializers.py
+++ apps/products/api/serializers.py
@@ -1,83 +1,76 @@
 from apps.products.models import Product, ProductImage
 from distutils.util import strtobool
-from apps.categories.models import  Category
+from apps.categories.models import Category
+from django.db import transaction
 
-
-class ProductImageSerializer:
-    class Meta:
-        model = ProductImage
-        fields = '__all__'
 
 class ProductSerializer:
     def create_product(self, request):
         try:
-            product = Product.objects.create(
-                name=request.data.get('name'),
-                category_id=request.data.get("category_id"),
-                cost=request.data.get('cost'),
-                inventory_number=request.data.get('inventory_number'),
-                favorite=eval(request.data.get('favorite').capitalize()),
-            )
+            with transaction.atomic():
+                product = Product.objects.create(
+                    name=request.data.get('name'),
+                    category_id=request.data.get("category_id"),
+                    cost=request.data.get('cost'),
+                    inventory_number=request.data.get('inventory_number'),
+                    favorite=eval(request.data.get('favorite').capitalize()),
+                )
 
-            self.add_images_to_product(product, request.data.getlist('images'))
+                self.add_images_to_product(product, request.data.getlist('images'))
 
-
-            return {'Product created successfully.'}, 201
+            return {"detail": "Product created successfully."}, 201
         except Exception as e:
             print(e)
-            return {'Product could not be created.'}, 400
+            return {"error": "'Product could not be created."}, 400
 
-
-    def get_product(self, product_pk):
-        product = Product.objects.get(pk=product_pk)
-        product_dic = {
-            "name": product.name,
-            "category": product.category_id,
-            "cost": product.cost,
-            "inventory_number": product.inventory_number,
-            "favorite":product.favorite,
-            "image":product.images.values(),
-        }
-
-        return product_dic, 200
-
+    def get_product(self, product):
+        product_dict = self.build_product_dict(product)
+        return product_dict, 200
 
     def update_product(self, product, request):
         try:
-            category = Category.objects.get(pk=int(request.data.get("category_id", product.category.pk)))
-            product.name = request.data.get("name", product.name)
-            product.category = category
-            product.cost = float(request.data.get("cost", product.cost))
-            product.inventory_number = float(request.data.get("inventory_number", product.inventory_number))
-            product.favorite = bool(strtobool(request.data.get('favorite', "false").capitalize()))
-            product.images.all().delete()
+            with transaction.atomic():
+                category = Category.objects.get(pk=int(request.data.get("category_id", product.category.pk)))
+                product.name = request.data.get("name", product.name)
+                product.category = category
+                product.cost = float(request.data.get("cost", product.cost))
+                product.inventory_number = float(request.data.get("inventory_number", product.inventory_number))
+                product.favorite = bool(strtobool(request.data.get('favorite', "false").capitalize()))
+                if request.data:
+                    images = request.data.getlist("images")
+                    if images:
+                        product.images.all().delete()
+                        self.add_images_to_product(product, images)
 
-            self.add_images_to_product(product, request.data.getlist("images"))
-
-            product.save()
-            return product, 200
+                product.save()
+                return {"detail": "Product was updated successfully."}, 201
         except Exception as e:
             print(e)
-            return {'Product could not be changed.'}, 400
+            return {"error": "Product could not be changed."}, 400
 
-
-    def build_product_dict(self, product):
+    @staticmethod
+    def build_product_dict(product):
+        category = product.category.get_root()
         product_dict = {
             "id": product.pk,
             "name": product.name,
             "category_id": product.category_id,
             "cost": product.cost,
             "inventory_number": product.inventory_number,
-            "favorite":product.favorite,
-            "image":product.images.values(),
+            "favorite": product.favorite,
+            "image": product.images.values(),
+            "category": {
+                "id": category.pk,
+                "name": category.name,
+            }
         }
-        return product_dict, 200
+        return product_dict
 
     def get_all_products_dict(self, products):
         product_list_dict = []
 
         for product in products:
-            product_dict, status = self.build_product_dict(product)
+            product_dict = self.build_product_dict(product)
             product_list_dict.append(product_dict)
 
         return product_list_dict, 200
--- apps/products/api/viewsets.py
+++ apps/products/api/viewsets.py
@@ -1,13 +1,15 @@
-from apps.products.models import Product, ProductImage
+from apps.products.models import Product
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
-from apps.products.api.serializers import ProductSerializer, ProductImageSerializer
+from apps.products.api.serializers import ProductSerializer
 from rest_framework.parsers import MultiPartParser
+from django.shortcuts import get_object_or_404
+
 
 class ProductList(APIView):
     serializer = ProductSerializer()
-    parser_classes = (MultiPartParser,) 
+    parser_classes = (MultiPartParser,)
+
     def get(self, request):
         if request.GET.get('q'):
             products = Product.objects.filter(search_text=request.GET.get('q'))
@@ -21,25 +23,18 @@ class ProductList(APIView):
                    
         response, status = self.serializer.create_product(request)
         return Response(response, status)
-       
 
 
 class ProductDetail(APIView):
-    parser_classes = (MultiPartParser,) 
+    parser_classes = (MultiPartParser,)
+    serializer = ProductSerializer()
     
     def get(self, request, product_pk):
-        serializer = ProductSerializer()
-        
-        response, status = serializer.get_product(product_pk)
+        product = get_object_or_404(Product, pk=product_pk)
+        response, status = self.serializer.get_product(product)
         return Response(response, status)
 
     def put(self, request, product_pk):
-        product = Product.objects.get(pk=product_pk)
-        serializer = ProductSerializer()
-        
-        response, status = serializer.update_product(product, request)
-        if status != 200:
-            return Response(response, status)
-
-        response, status = serializer.build_product_dict(response)
+        product = get_object_or_404(Product, pk=product_pk)
+        response, status = self.serializer.update_product(product, request)
         return Response(response, status)
